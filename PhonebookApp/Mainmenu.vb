cf6e02a7473cd8379ae1ec5f9042eb458e690e5e
kbradburn <kcbradburn98@gmail.com>
2021-09-30 11:50:10 -0500

    Implemented interface and class

---
+++ PhonebookApp/Mainmenu.vb
@@ -0,0 +1,20 @@
+Class MainMenu
+    Implements IMenu
+    Sub Start() Implements IMenu.Start
+        Console.WriteLine("Hello World!")
+        dim repeat as Boolean = True 
+        Do While repeat
+            Console.WriteLine("[0] Say hello")
+            Console.WriteLine("[x] Exit")
+            Dim input as string = Console.Readline()
+            Select Case input
+                Case "0"
+                    Console.WriteLine("Hello")
+                Case "x"
+                    Console.WriteLine("goodbye")
+                    repeat = false
+            End Select
+        Loop
+    End Sub
+    
+End Class
--- PhonebookApp/Program.vb
+++ PhonebookApp/Program.vb
@@ -2,19 +2,7 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
-        dim repeat as Boolean = True 
-        Do While repeat
-            Console.WriteLine("[0] Say hello")
-            Console.WriteLine("[x] Exit")
-            Dim input as string = Console.Readline()
-            Select Case input
-                Case "0"
-                    Console.WriteLine("Hello")
-                Case "x"
-                    Console.WriteLine("goodbye")
-                    repeat = false
-            End Select
-        Loop
+        Dim menu as imenu = new MainMenu()
+        menu.Start()
     End Sub
 End Module
