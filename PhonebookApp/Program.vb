7cc2e74475ca8311d378ca208b4bbb5d8be2259d
kbradburn <kcbradburn98@gmail.com>
2021-09-30 10:53:16 -0500

    added first test of VB.Net code

--- PhonebookApp/Program.vb
+++ PhonebookApp/Program.vb
@@ -3,5 +3,18 @@ Imports System
 Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
+        dim repeat as Boolean = True 
+        Do While repeat
+            Console.WriteLine("[0] Say hello")
+            Console.WriteLine("[x] Exit")
+            Dim input as string = Console.Readline()
+            Select Case input
+                Case "0"
+                    Console.WriteLine("Hello")
+                Case "x"
+                    Console.WriteLine("goodbye")
+                    repeat = false
+            End Select
+        Loop
     End Sub
 End Module
